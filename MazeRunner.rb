6361b4c9f47206e5730bab3852fc3b780d1d7941
App Academy Student <student@appacademy.io>
2015-02-17 20:55:20 -0800

    solves the maze

--- MazeRunner.rb
+++ MazeRunner.rb
@@ -1,61 +1,66 @@
-class Array
-  def p(pt)
-    self[pt[:y]][pt[:x]]
+require 'byebug'
+
+class MazeUnit
+
+  def initialize(char)
+    @parent = nil
+    @char = char
   end
+
+  attr_accessor :char, :parent
 end
 
+def loadMaze(file)
+  maze = []
+  mazelines = File.readlines(file)
 
-maze = []
-start_point = {}
+  width = mazelines[0].length
+  height = mazelines.length
 
+  start_point = -1
+  end_point = -1
 
-File.foreach ('maze1.txt') do |line|
-  maze.push(line.split(//))
-end
-
-discovered = Array.new(maze.length) { Array.new(maze[0].length, false) }
-parents = Array.new(maze.length) { Array.new(maze[0].length, nil) }
-
-
-# find start
-
-maze.each_with_index do |line, l_index|
-  line.each_with_index do |obj, o_index|
-    start_point = {x: o_index, y: l_index} if obj == "S"
-  end
-end
-
-proc_queue = [start_point]
-p start_point
-discovered[start_point[:y]][start_point[:x]] = 1
-
-
-def wewin(new_point, maze, parents)
-  win_maze = maze.clone
-  draw_point = (new_point)
-  until parents[draw_point[:y]][draw_point[:x]] == nil
-    win_maze[draw_point[:y]][draw_point[:x]] = "X"
-    draw_point = parents[draw_point[:y]][draw_point[:x]]
-  end
-  win_maze.each do |line|
-    puts line.join("")
-  end
-end
-
-
-loop do
-  current_point = proc_queue.shift
-  [{x:0,y:1},{x:1,y:0},{x:0,y:-1},{x:-1,y:0}].each do |dirvector|
-    new_point = {x: current_point[:x] + dirvector[:x], y: current_point[:y] + dirvector[:y]}
-    if maze[new_point[:y]][new_point[:x]] = "E"
-      parents[new_point[:y]][new_point[:x]] = current_point
-      wewin(new_point, maze, parents)
-      break
-    end
-    if !discovered[new_point[:y]][new_point[:x]] and maze[new_point[:y]][new_point[:x]] = " "
-      discovered[new_point[:y]][new_point[:x]] = discovered[current_point[:y]][current_point[:x]] + 1
-      parents[new_point[:y]][new_point[:x]] = current_point
-      proc_queue.push(new_point)
+  mazelines.each do |line|
+    line.split(//).each do |char|
+      maze.push(MazeUnit.new(char))
+      start_point = maze.length - 1 if char == "S"
+      end_point = maze.length - 1 if char == "E"
     end
   end
+
+  return maze, start_point, end_point, width, height
+end
+
+def checkPoint(point)
+  point.parent == nil and ['E',' '].include?(point.char)
+end
+
+
+def fillMaze(maze, start_point, width)
+  proc_queue = [start_point]
+  while current_point = proc_queue.shift
+    [-1, 1, -width, width].each do |direction|
+      new_point = current_point + direction
+      if checkPoint(maze[new_point])
+        proc_queue.push(new_point)
+        maze[new_point].parent = current_point
+      end
+    end
+  end
+end
+
+def printSolution(maze, end_point, width, height)
+  current_point = end_point
+  while current_point = maze[current_point].parent
+    maze[current_point].char = "X" unless maze[current_point].char == "S"
+  end
+  (0..height).each do |line|
+    puts maze[line * width, width].map { |mazeunit| mazeunit.char }.join("")
+  end
+end
+
+def solveMaze(file)
+  maze, start_point, end_point, width, height = loadMaze(file)
+  fillMaze(maze, start_point, width)
+  printSolution(maze, end_point, width, height)
 end
